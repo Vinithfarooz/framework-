a3d1f6d99e8ef053901a95ba97b721a19e0aab48
Vinithfarooz <vinithfarooz25@gmail.com>
2021-12-31 19:33:05 +0530

    Username Functionality

---
+++ src/org/com/Newyear.java
@@ -0,0 +1,10 @@
+package org.com;
+
+public class Newyear {
+	private void Password() {
+		System.out.println("Password");
+	}
+public static void main(String[] args) {
+	
+}
+}
---
+++ src/org/com/sam.java
@@ -0,0 +1,11 @@
+package org.com;
+
+public class sam {
+	private void username() {
+System.out.println("Username");
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
